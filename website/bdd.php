3be820460747465847055db33466b29f3f8a7ba2
Liam Cariou <cariou.liam@orange.fr>
2022-12-17 19:06:43 +0100

    envoie dans bdd ok

--- website/bdd.php
+++ website/bdd.php
@@ -1,12 +1,13 @@
 <form method="POST" action="">
-    <input type = "hidden" value="" name="produit">
+    <input type = "hidden" value= "" name="produit">
     <input type ="submit" name ="btnproduit" value = "Envoyer les produits">
 </form>
 
 
+
 <?php
 
-    if(isset($_POST['btnproduit'])) {
+    if(isset($_POST['btnproduit']) && isset($_POST['produit'])) {
         // Connection variables
         $host = 'localhost';
         $user = 'root';
@@ -24,21 +25,20 @@
 
         if($_POST['produit']){
             // Read the contents of the JSON file
-            $json = file_get_contents('pageacc.json');
+            $json = file_get_contents('../pageacc.json');
             $data = json_decode($json);
 
             // Insert the data into the database
             foreach ($data as $row) {
-                $sql = "INSERT INTO produit (titre, img, sale, lien) VALUES (:titre, :img, :sale, :lien)";
+                $sql = "INSERT INTO produit (titre, img, sale, lien) VALUES (:value1, :value2, :value3, :value4)";
                 $stmt = $db->prepare($sql);
                 $stmt->execute([
-                    'titre' => $row->titre,
-                    'img' => $row->img,
-                    'sale' => $row->sale,
-                    'lien' => $row->lien
+                    'value1' => $row->titre,
+                    'value2' => $row->img,
+                    'value3' => $row->sale,
+                    'value4' => $row->lien,
                 ]);
             }
         }
-     }
 
-        ?>
+    }
